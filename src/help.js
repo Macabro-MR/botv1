b3cde53c43b90abec6980621a17f20f6ff77e681
Macabro-MR <maniimroots391@gmail.com>
2021-05-12 03:53:03 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,66 @@
+const help = (prefix) => {
+	return `
+「 *🌺·᳢᳢᳔᳢᳕᳖᳖᳗᳟֗᳚᳛ᱺ٬̤⃟⃢𝐸𝑅𝑌𝑆-𝐵❍⃢⃟᷂᷂᷂᷂᷂᳜᳝᳖᳕᳕᷁᷁᷁᷈᷈᷀᷀᷃᷃᷄᷄᷅᷅᷆᷉᷑᷇᷇᷆᷆᷆᷅᷄᷃᷁᳛᷍𝑇😈* 」
+
+◪ *Olá ${pushname} Bem-vindo(a)*
+
+◪ *SOBRE*
+  ❏ *Prefixo:* 「  ${prefix}  」
+  ❏ *Versão da 🌺·᳢᳢᳔᳢᳕᳖᳖᳗᳟֗᳚᳛ᱺ٬̤⃟⃢𝐸𝑅𝑌𝑆-𝐵❍⃢⃟᷂᷂᷂᷂᷂᳜᳝᳖᳕᳕᷁᷁᷁᷈᷈᷀᷀᷃᷃᷄᷄᷅᷅᷆᷉᷑᷇᷇᷆᷆᷆᷅᷄᷃᷁᳛᷍𝑇😈:* 2.0.0
+  ❏ *Dono:* wa.me/+5548984555584
+◪ *MÚSICA*
+  │
+  └─ ❏ ${prefix}play (nome da música)
+◪ *STICKER*
+  │
+  ├─ ❏ ${prefix}s
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}img
+  └─ ❏ ${prefix}fga
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}clearall
+  ├─ ❏ ${prefix}sticker
+  └─ ❏ ${prefix}gay
+◪ *ADMS*
+  │
+  ├─ ❏ ${prefix}welcome
+  ├─ ❏ ${prefix}link
+  ├─ ❏ ${prefix}del
+  ├─ ❏ ${prefix}demitir
+  ├─ ❏ ${prefix}sair
+  ├─ ❏ ${prefix}abrir
+  ├─ ❏ ${prefix}fechar
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}promover
+  ├─ ❏ ${prefix}membros
+  └─ ❏ ${prefix}notif
+◪ *PRODUTOS*
+  │
+  ❏ Seg: Brasileiros e Mundiais
+  ❏ Inscritos: Brasileiros e Mundiais
+  ❏ Likes: Brasileiros e Mundiais
+  ❏ Views: Brasileiros e Mundiais
+  ❏ May: premium
+◪ *PLATAFORMAS*
+  │
+  ❏ Instagram
+  ❏ Youtube
+  ❏ Tiktok
+  ❏ Twitch
+  ❏ Whatsapp
+◪ *CRÉDITOS*
+  │
+  ❏ *Criador:* ✦҈͜͡✧͢⃟👿𝐌𝐀𝐂𝐀𝚩𝐑𝐎¹⁵⁷
+  ❏ *Instagram:* hk.macabro
+  ❏ *Voz da Erys:* Rafaella
+  ❏ *Instagram:* @erys.roots
+  ❏ Site: priv8.com.br (off)
+  ❏ Auxílio: Kill
+  ❏ Github: github.com/KillovSky
+  ❏ Animes: Haru a
+  ❏ Instagram: @blue_.ghost_
+`
+}
+
+exports.help = help
